f9a54b2980cb2b30f4828ac2903ee00408f7f723
claydersons <watashisousa@hotmail.com>
2018-04-15 13:37:50 -0300

    Update vpsmanager_centos.sh

--- vpsmanager_centos.sh
+++ vpsmanager_centos.sh
@@ -44,79 +44,20 @@ yum update -y
 yum upgrade -y
 rm -f /bin/criarusuario /bin/expcleaner /bin/sshlimiter /bin/addhost /bin/listar /bin/sshmonitor /bin/ajuda > /dev/null
 rm -f /root/ExpCleaner.sh /root/CriarUsuario.sh /root/sshlimiter.sh > /dev/null
-yum install squid3 bc screen nano unzip dos2unix wget -y
+yum install squid bc screen nano unzip dos2unix wget -y
 
 
 if [ -f "/usr/sbin/ufw" ] ; then
 	ufw allow 443/tcp ; ufw allow 80/tcp ; ufw allow 3128/tcp ; ufw allow 8799/tcp ; ufw allow 8080/tcp
-fi
-if [ -d "/etc/squid3/" ]
-then
-	wget http://phreaker56.obex.pw/vpsmanager/squid1.txt -O /tmp/sqd1
-	echo "acl url3 dstdomain -i $ipdovps" > /tmp/sqd2
-	wget http://phreaker56.obex.pw/vpsmanager/squid2.txt -O /tmp/sqd3
-	cat /tmp/sqd1 /tmp/sqd2 /tmp/sqd3 > /etc/squid3/squid.conf
-	wget http://phreaker56.obex.pw/vpsmanager/payload.txt -O /etc/squid3/payload.txt
-	echo " " >> /etc/squid3/payload.txt
-	grep -v "^Port 443" /etc/ssh/sshd_config > /tmp/ssh && mv /tmp/ssh /etc/ssh/sshd_config
-	echo "Port 443" >> /etc/ssh/sshd_config
-	grep -v "^PasswordAuthentication yes" /etc/ssh/sshd_config > /tmp/passlogin && mv /tmp/passlogin /etc/ssh/sshd_config
-	echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
-	wget https://raw.githubusercontent.com/claydersons/vpspinda/master/addhost.sh -O /bin/addhost
-	chmod +x /bin/addhost
-	wget https://raw.githubusercontent.com/claydersons/vpspinda/master/ajuda.sh -O /bin/ajuda
-	chmod +x /bin/ajuda
-	wget https://raw.githubusercontent.com/claydersons/vpspinda/master/alterarlimite.sh -O /bin/alterarlimite
-	chmod +x /bin/alterarlimite
-	wget https://raw.githubusercontent.com/claydersons/vpspinda/master/alterarsenha.sh -O /bin/alterarsenha
-	chmod +x /bin/alterarsenha
-	wget https://raw.githubusercontent.com/claydersons/vpspinda/master/delhost.sh -O /bin/delhost
-	chmod +x /bin/delhost
-	wget https://raw.githubusercontent.com/claydersons/vpspinda/master/criarusuario2.sh -O /bin/criarusuario
-	chmod +x /bin/criarusuario
-	wget https://raw.githubusercontent.com/claydersons/vpspinda/master/expcleaner2.sh -O /bin/expcleaner
-	chmod +x /bin/expcleaner
-	wget https://raw.githubusercontent.com/claydersons/vpspinda/master/mudardata.sh -O /bin/mudardata
-	chmod +x /bin/mudardata
-	wget https://raw.githubusercontent.com/claydersons/vpspinda/master/remover.sh -O /bin/remover
-	chmod +x /bin/remover
-	wget https://raw.githubusercontent.com/claydersons/vpspinda/master/sshlimiter2.sh -O /bin/sshlimiter
-	chmod +x /bin/sshlimiter
-	wget https://raw.githubusercontent.com/claydersons/vpspinda/master/sshmonitor2.sh -O /bin/sshmonitor
-	chmod +x /bin/sshmonitor
-	wget https://raw.githubusercontent.com/claydersons/MNHVPSSSL/master/ssltunnel.sh -O /bin/ssltunnel
-	chmod +x /bin/ssltunnel
-	wget https://raw.githubusercontent.com/claydersons/MNHVPSSSL/master/stunnel.sh -O /bin/stunnel
-	chmod +x /bin/stunnel
-	wget https://raw.githubusercontent.com/claydersons/MNHVPSSSL/master/stunnel2.sh -O /bin/stunnel2
-	chmod +x /bin/stunnel2
-	wget https://raw.githubusercontent.com/claydersons/MNHVPSSSL/master/stunnel4b0.sh -O /bin/stunnel4b0
-	chmod +x /bin/stunnel4b0
-	wget https://raw.githubusercontent.com/claydersons/MNHVPSSSL/master/stunnel4b1.sh -O /bin/stunnel4b1
-	chmod +x /bin/stunnel4b1
-	wget https://raw.githubusercontent.com/claydersons/MNHVPSSSL/master/desinstalarstunnel4.sh -O /bin/desinstalarstunnel4
-	chmod +x /bin/desinstalarstunnel4
-	if [ ! -f "/etc/init.d/squid3" ]
-	then
-		service squid3 reload > /dev/null
-	else
-		/etc/init.d/squid3 reload > /dev/null
-	fi
-	if [ ! -f "/etc/init.d/ssh" ]
-	then
-		service ssh reload > /dev/null
-	else
-		/etc/init.d/ssh reload > /dev/null
-	fi
 fi
 if [ -d "/etc/squid/" ]
 then
 	wget http://phreaker56.obex.pw/vpsmanager/squid1.txt -O /tmp/sqd1
 	echo "acl url3 dstdomain -i $ipdovps" > /tmp/sqd2
 	wget http://phreaker56.obex.pw/vpsmanager/squid2.txt -O /tmp/sqd3
-	cat /tmp/sqd1 /tmp/sqd2 /tmp/sqd3 > /etc/squid3/squid.conf
-	wget http://phreaker56.obex.pw/vpsmanager/payload.txt -O /etc/squid3/payload.txt
-	echo " " >> /etc/squid3/payload.txt
+	cat /tmp/sqd1 /tmp/sqd2 /tmp/sqd3 > /etc/squid/squid.conf
+	wget http://phreaker56.obex.pw/vpsmanager/payload.txt -O /etc/squid/payload.txt
+	echo " " >> /etc/squid/payload.txt
 	grep -v "^Port 443" /etc/ssh/sshd_config > /tmp/ssh && mv /tmp/ssh /etc/ssh/sshd_config
 	echo "Port 443" >> /etc/ssh/sshd_config
 	grep -v "^PasswordAuthentication yes" /etc/ssh/sshd_config > /tmp/passlogin && mv /tmp/passlogin /etc/ssh/sshd_config
@@ -157,9 +98,68 @@ then
 	chmod +x /bin/desinstalarstunnel4
 	if [ ! -f "/etc/init.d/squid" ]
 	then
-		service squid3 reload > /dev/null
+		service squid reload > /dev/null
 	else
-		/etc/init.d/squid3 reload > /dev/null
+		/etc/init.d/squid reload > /dev/null
+	fi
+	if [ ! -f "/etc/init.d/ssh" ]
+	then
+		service ssh reload > /dev/null
+	else
+		/etc/init.d/ssh reload > /dev/null
+	fi
+fi
+if [ -d "/etc/squid/" ]
+then
+	wget http://phreaker56.obex.pw/vpsmanager/squid1.txt -O /tmp/sqd1
+	echo "acl url3 dstdomain -i $ipdovps" > /tmp/sqd2
+	wget http://phreaker56.obex.pw/vpsmanager/squid2.txt -O /tmp/sqd3
+	cat /tmp/sqd1 /tmp/sqd2 /tmp/sqd3 > /etc/squid/squid.conf
+	wget http://phreaker56.obex.pw/vpsmanager/payload.txt -O /etc/squid/payload.txt
+	echo " " >> /etc/squid/payload.txt
+	grep -v "^Port 443" /etc/ssh/sshd_config > /tmp/ssh && mv /tmp/ssh /etc/ssh/sshd_config
+	echo "Port 443" >> /etc/ssh/sshd_config
+	grep -v "^PasswordAuthentication yes" /etc/ssh/sshd_config > /tmp/passlogin && mv /tmp/passlogin /etc/ssh/sshd_config
+	echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+	wget https://raw.githubusercontent.com/claydersons/vpspinda/master/addhost.sh -O /bin/addhost
+	chmod +x /bin/addhost
+	wget https://raw.githubusercontent.com/claydersons/vpspinda/master/ajuda.sh -O /bin/ajuda
+	chmod +x /bin/ajuda
+	wget https://raw.githubusercontent.com/claydersons/vpspinda/master/alterarlimite.sh -O /bin/alterarlimite
+	chmod +x /bin/alterarlimite
+	wget https://raw.githubusercontent.com/claydersons/vpspinda/master/alterarsenha.sh -O /bin/alterarsenha
+	chmod +x /bin/alterarsenha
+	wget https://raw.githubusercontent.com/claydersons/vpspinda/master/delhost.sh -O /bin/delhost
+	chmod +x /bin/delhost
+	wget https://raw.githubusercontent.com/claydersons/vpspinda/master/criarusuario2.sh -O /bin/criarusuario
+	chmod +x /bin/criarusuario
+	wget https://raw.githubusercontent.com/claydersons/vpspinda/master/expcleaner2.sh -O /bin/expcleaner
+	chmod +x /bin/expcleaner
+	wget https://raw.githubusercontent.com/claydersons/vpspinda/master/mudardata.sh -O /bin/mudardata
+	chmod +x /bin/mudardata
+	wget https://raw.githubusercontent.com/claydersons/vpspinda/master/remover.sh -O /bin/remover
+	chmod +x /bin/remover
+	wget https://raw.githubusercontent.com/claydersons/vpspinda/master/sshlimiter2.sh -O /bin/sshlimiter
+	chmod +x /bin/sshlimiter
+	wget https://raw.githubusercontent.com/claydersons/vpspinda/master/sshmonitor2.sh -O /bin/sshmonitor
+	chmod +x /bin/sshmonitor
+	wget https://raw.githubusercontent.com/claydersons/MNHVPSSSL/master/ssltunnel.sh -O /bin/ssltunnel
+	chmod +x /bin/ssltunnel
+	wget https://raw.githubusercontent.com/claydersons/MNHVPSSSL/master/stunnel.sh -O /bin/stunnel
+	chmod +x /bin/stunnel
+	wget https://raw.githubusercontent.com/claydersons/MNHVPSSSL/master/stunnel2.sh -O /bin/stunnel2
+	chmod +x /bin/stunnel2
+	wget https://raw.githubusercontent.com/claydersons/MNHVPSSSL/master/stunnel4b0.sh -O /bin/stunnel4b0
+	chmod +x /bin/stunnel4b0
+	wget https://raw.githubusercontent.com/claydersons/MNHVPSSSL/master/stunnel4b1.sh -O /bin/stunnel4b1
+	chmod +x /bin/stunnel4b1
+	wget https://raw.githubusercontent.com/claydersons/MNHVPSSSL/master/desinstalarstunnel4.sh -O /bin/desinstalarstunnel4
+	chmod +x /bin/desinstalarstunnel4
+	if [ ! -f "/etc/init.d/squid" ]
+	then
+		service squid reload > /dev/null
+	else
+		/etc/init.d/squid reload > /dev/null
 	fi
 	if [ ! -f "/etc/init.d/ssh" ]
 	then
